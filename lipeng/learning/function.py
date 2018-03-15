eeabbd71bc7e9e2986180929eef35bfc23628f0b
李朋 <lipeng@1fangxin.cn>
2018-03-15 23:03:23 +0800

    提交机器学习测试

---
+++ lipeng/learning/function.py
@@ -0,0 +1,17 @@
+# 测试内置函数
+print(22)
+
+
+def cylinder_volume(height, radius):
+    pi = 3.14159
+    return height * pi * radius ** 2
+
+
+print(cylinder_volume(20, 10))
+
+
+def print_greeting():
+    print('Hello world')
+
+
+print_greeting()
