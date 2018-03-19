f6c3bdabf41e49a1c3c0e9edadf08a0ae7d80ad1
韩文博 <hanwenbo@1fangxin.cn>
2018-03-19 22:20:40 +0800

    完成部分用例

--- hanwenbo/investigate_texts_and_calls/Task0.py
+++ hanwenbo/investigate_texts_and_calls/Task0.py
@@ -3,6 +3,7 @@
 你将在以后的课程中了解更多有关读取文件的知识。
 """
 import csv
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -10,7 +11,6 @@ with open('texts.csv', 'r') as f:
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
-
 
 """
 任务0:
@@ -20,3 +20,20 @@ with open('calls.csv', 'r') as f:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+
+def print_text(data, index):
+    text = data[index]
+    print(
+        "First record of texts, {} texts {} at time {}".format(text[0], text[1], text[2]))
+
+
+def print_call(data, index):
+    call = data[index]
+    print(
+        "Last record of calls, {} calls {} at time {}, lasting {} seconds".format(call[0], call[1],
+                                                                                  call[2], call[3]))
+
+
+print_text(texts, 0)
+
+print_call(calls, -1)
--- hanwenbo/test.py
+++ hanwenbo/test.py
@@ -1,1 +1,16 @@
-print(4+1);
+how_many_snakes = 3
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Philip and Charlie
+"""
+
+
+print(snake_string * how_many_snakes)
