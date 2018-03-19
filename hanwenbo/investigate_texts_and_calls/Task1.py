5e8099a23010eceb58f907ff0dfe6a13fafa95a8
韩文博 <hanwenbo@1fangxin.cn>
2018-03-19 09:20:05 +0800

    init

---
+++ hanwenbo/investigate_texts_and_calls/Task1.py
@@ -0,0 +1,19 @@
+"""
+下面的文件将会从csv文件中读取读取短信与电话记录，
+你将在以后的课程中了解更多有关读取文件的知识。
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+
+"""
+任务1：
+短信和通话记录中一共有多少电话号码？每个号码只统计一次。
+输出信息：
+"There are <count> different telephone numbers in the records.""""
