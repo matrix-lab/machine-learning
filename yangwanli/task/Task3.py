007b5b7cec98f770a03189218ce0e69f3478cd57
杨婉丽 <15538093733@163.com>
2018-03-26 17:36:27 +0800

    机器学习

--- yangwanli/task/Task3.py
+++ yangwanli/task/Task3.py
@@ -16,7 +16,7 @@ with open('calls.csv', 'r') as f:
 任务3:
 (080)是班加罗尔的固定电话区号。
 固定电话号码包含括号，
-所以班加罗尔地区的电话号码的格式为(080)xxxxxxx。
+所以班加罗尔地区的电话号码的格式为(080)xxxxxxxx。
 
 第一部分: 找出被班加罗尔地区的固定电话所拨打的所有电话的区号和移动前缀（代号）。
  - 固定电话以括号内的区号开始。区号的长度不定，但总是以 0 打头。
@@ -39,3 +39,41 @@ with open('calls.csv', 'r') as f:
 to other fixed lines in Bangalore."
 注意：百分比应包含2位小数。
 """
+
+import re
+calls_dic = {}
+"""
+判断代号并存储：被拨打的电话，作为键，如果已存在，则不存储在字典中，否则存储
+"""
+def is_code(data):
+    if re.match(r'^\(0',data):
+        return data[1:data.index(')')]
+    elif re.match(r'^[7,8,9]',data):
+        return data[0:4]
+    elif re.match(r'^140',data):
+        return '140'
+
+
+for call in calls:
+    if re.match(r'\(080\)[0-9]{8}',call[0]):
+        if is_code(call[1]) not in calls_dic:
+            calls_dic[is_code(call[1])]=call[1]
+
+call_str='The numbers called by people in Bangalore have codes:'
+def print_result(dicts):
+    str = '\n'.join(dicts)
+    print(call_str + str)
+
+print_result(calls_dic)
+
+calls_from_ban=[]
+calls_to_ban=[]
+for call in calls:
+    if re.match(r'\(080\)[0-9]{8}',call[0]):
+        calls_from_ban.append(call)
+
+for call in calls_from_ban:
+    if re.match(r'\(080\)[0-9]{8}',call[1]):
+        calls_to_ban.append(call)
+
+print('{:.2f}percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore.'.format((len(calls_to_ban))/len(calls_from_ban)))
