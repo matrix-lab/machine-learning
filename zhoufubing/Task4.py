958da2fc0c16cf1b45510418cf3453da8afbdc49
zfb1993 <871724075@qq.com>
2018-04-02 21:53:15 +0800

    'task4'

--- zhoufubing/Task4.py
+++ zhoufubing/Task4.py
@@ -26,3 +26,25 @@ with open('calls.csv', 'r') as f:
 电话号码不能重复，每行打印一条，按字典顺序排序后输出。
 """
 
+def get_num(calls,texts):
+	notT = set()
+	t = set()
+	for text in texts:
+		notT.add(text[0])
+		notT.add(text[1])
+
+	for call in calls:
+		notT.add(call[1])
+
+	for ca in calls:
+		if ca[0] not in notT:
+			t.add(ca[0])
+
+	return sorted(list(t))
+
+print("These numbers could be telemarketers: ")
+
+num = get_num(calls,texts)
+
+for phone in num:
+	print(phone)
