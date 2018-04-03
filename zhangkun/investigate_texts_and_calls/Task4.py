11f67f280cdc33eb1a4989099ca21d1cfa4f1ebf
张坤 <zhkugh@163.com>
2018-04-03 15:16:29 +0800

    update task4

--- zhangkun/investigate_texts_and_calls/Task4.py
+++ zhangkun/investigate_texts_and_calls/Task4.py
@@ -1,7 +1,5 @@
-"""
-下面的文件将会从csv文件中读取读取短信与电话记录，
-你将在以后的课程中了解更多有关读取文件的知识。
-"""
+# -*- coding: utf-8 -*-
+
 import csv
 
 with open('texts.csv', 'r') as f:
@@ -12,17 +10,25 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-"""
-任务4:
-电话公司希望辨认出可能正在用于进行电话推销的电话号码。
-找出所有可能的电话推销员:
-这样的电话总是向其他人拨出电话，
-但从来不发短信、接收短信或是收到来电
+t_tels = set()
+c_tels = set()
+s_tels = set()
+
+for text in texts:
+    t_tels.add(text[0])
+    t_tels.add(text[1])
+
+for call in calls:
+    c_tels.add(call[1])
 
 
-请输出如下内容
-"These numbers could be telemarketers: "
-<list of numbers>
-电话号码不能重复，每行打印一条，按字典顺序排序后输出。
-"""
+for call in calls:
+    if call[0] not in t_tels and call[0] not in c_tels:
+        s_tels.add(call[0])
+
+print("These numbers could be telemarketers: ")
+
+for tel in sorted(list(s_tels)):
+    print(tel)
+
 
